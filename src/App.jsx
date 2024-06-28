6900a6de1006aed807ed537dd0c364404961806a
YashaswiSV <vermayashaswi1@gmail.com>
2024-06-28 15:47:28 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,389 +1,228 @@
 
-// import Header from "./Header";
-// import Middle from "./Middle";
-// import Footer from "./Footer";
+// ==================================================================
+
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+import Carousel from 'react-bootstrap/Carousel';
 
 
+import Card from 'react-bootstrap/Card';
 
-// const App=()=>{
-//   return(
-//     <>
-    
-//     <h1>Welcome to cybrom !!!!!</h1>
-//     <Header/>
-//     <Middle/>
-//     <Footer/>
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
-//     </>
-//   )
-// }
+import Image from 'react-bootstrap/Image';
 
-// export default App;
-// ==================================================
-// const age=45;
-// const name="Riya";
 
-// ==============================================
-// const sub=<ul>
-//    <li>PHP</li>
-//    <li>Java</li>
-//    <li>Asp</li>
-//    <li>Oracle</li>
+const App=()=>{
+  return(
+    <>
+          <Navbar expand="lg" className="bg-body-dark" bg="light">
+      <Container fluid>
+        <Navbar.Brand href="#"><img src="public/logo/l4.png" alt="" width="50px" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <Nav.Link href="#action1">Home</Nav.Link>
+            <Nav.Link href="#action2">Link</Nav.Link>
+            <NavDropdown title="Link" id="navbarScrollingDropdown">
+              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action4">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action5">
+                Something else here
+              </NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link href="#" disabled>
+              Link
+            </Nav.Link>
+          </Nav>
+          <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
 
-// </ul>
 
-// const App=()=>{
-//     return(
-//       <>
-      
-//       <h1>hello my subject is : {sub}</h1>
-    
+    <Carousel>
+      <Carousel.Item>
+
+         <img src="public/1.jpg" alt="" width="100%"  height="600px"/>    
   
-//       </>
-//     )
-//   }
-// export default App;
+        <Carousel.Caption>
+          <h3 >Feel like new home</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
 
-// =========================================================
-/*
-import Header from "./Header";
-import Data from "./Data";
-import Footer from "./Footer";
+      <img src="public/2.jpg" alt=""  width="100%"  height="600px" />
+        <Carousel.Caption>
+          <h3>Design your way</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+      <img src="public/4.jpg" alt=""  width="100%"  height="600px" />
+        <Carousel.Caption>
+          <h3>Customize your home</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
 
-const App=()=>{
-    return(
-      <>
-      
-        
-        
-        <Header/>
-         <hr />
-        <Data/>
-        <hr />
-        <Footer/>
-  
-      </>
-    )
-  }
-export default App;
+      <Carousel.Item>
+      <img src="public/10.jpg" alt=""  width="100%"  height="600px" />
+        <Carousel.Caption>
+          <h3  >Sweet home</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+{/* { ------------------------------------------------------------------------------------------------------  */}
 
-*/
-// ==================================
-/*
-import Employee from "./Employee";
-
-const App=()=>{
-    return(
-      <>
-      <h1> Details of Employee: </h1>
-      <Employee nm="Raj" empno="501" sal="32000"/>
-        
-        
-        
-      </>
-    )
-  }
-export default App;
-*/
-
-// =====================================================
-/*
-import Student from "./Student";
-
-const nm="riya";
-const cls="mtech";
-const fees="32000";
-
-const App=()=>{
-    return(
-      <>
-      <h1> Details of Student: </h1>
-      <Student nm={nm} cls={cls} fee={fees}/>
-        
-        
-        
-      </>
-    )
-  }
-export default App;
-*/
-// =======================================================================================
-// =========================20 june==(using object)================
-/*
-import Cybrom from "./Cybrom"
-
-const student={                                 //props using object
-  "rollno":1221,
-  "name":"Tanya",
-  "city":"Bhopal"
-
-}
-
-const App=()=>{
-  return(
-    <>
-    <h1>Welcome to cybrom class bhopal</h1>
-     <Cybrom rno={student.rollno} nm={student.name} city={student.city}/>
-    </>
-  )
-}
-export default App;
-*/
-// ===================================Map======================================
-
-// const App=()=>{
-
-//   const name=["ram","shyam","mohan","sohan"];
-//   const ans=name.map((key)=>{
-//     return(
-//       <>
-//        <h1>{key}</h1>
-//       </>
-//     )
-//   })
-
-//   return(
-//     <>
-//     <h1>List of student</h1>
-//     {ans}
-//     </>
-//   )
-// }
-
-// export default App;
-
-// =============================================================
-/*
-const name=[30,50,76,90];
-const ans=name.map((key)=>{
-  return(
-    <>
-     <h1>{key*100}</h1>
-    </>
-  )
-})
-const App=()=>{
-  return(
-    <>
-    <h1>List of number</h1>
-    {ans}
-    </>
-  )
-}
-
-export default App;
-*/
-
-//====================================================================
-/*
-const city=["bhopal","goa","shimla","manali","gwalior","ujjain"];
-
-const ans=city.map((key)=>{
-  return(
-    <>
-     <option>
-      {key}
-     </option>
-    </>
-  )
-})
-const App=()=>{
-  return(
-    <>
-    <h1>List of city</h1>
-    <select >
-    {ans}
-    </select>
-    
-    </>
-  )
-}
-
-export default App;
-*/
-// =================================================================
-/*
-const subject=["php","oracle","java","css","html","react","python"];
-
-const App=()=>{
-const mysub=subject.map((key)=>{
-  return(
-    <>
-     <li>
-      {key}
-     </li>
-    </>
-  )
-
-})
-return(
-  <>
-  <h1>List of city</h1>
-  <ol>
-  {mysub}
-  </ol>
-  
-  </>
-)
-}
-export default App;
-
-*/
-// ======================================21 June==================================
-/*
-const student=[
-  {
-    rollno :121,
-    name:"Rama",
-    city:"Bhopal"
-  },
-  {
-    rollno :122,
-    name:"sanju",
-    city:"Indore"
-  },{
-    rollno :123,
-    name:"priya",
-    city:"Goa"
-  },
-  {
-    rollno :124,
-    name:"yuvi",
-    city:"gwalior"
-  }
-]
-
-const ans=student.map((key)=>{
-     return(
-      <>
-        
-          <tr>
-               <td>{key.rollno}  </td>
-               <td>{key.name}  </td>
-               <td>{key.city}  </td>
-
-          </tr>
+    <Container>
+    <Row className='my-5'>
+        <Col >
+        <Card style={{ width: '18rem' }}>
+          <img src="public/m4.avif" alt="" width="300px" height="200px"/>
+<Card.Body>
+  <Card.Title>Card Title</Card.Title>
+  <Card.Text>
+    Some quick example text to build on the card title and make up the
+    bulk of the card's content.
+  </Card.Text>
+  <Button variant="primary">Go somewhere</Button>
+</Card.Body>
+</Card>
           
+        </Col>
+        <Col>
+        <Card style={{ width: '18rem'  }}>
+        <img src="public/m5.avif" alt="" width="300px" height="200px"/>
+
+<Card.Body>
+  <Card.Title>Card Title</Card.Title>
+  <Card.Text>
+    Some quick example text to build on the card title and make up the
+    bulk of the card's content.
+  </Card.Text>
+  <Button variant="primary">Go somewhere</Button>
+</Card.Body>
+</Card>
+        </Col>
+
+        <Col>
+        <Card style={{ width: '18rem' }}>
+        <img src="public/m6.avif" alt="" width="300px"  />
+
+<Card.Body>
+  <Card.Title>Card Title</Card.Title>
+  <Card.Text>
+    Some quick example text to build on the card title and make up the
+    bulk of the card's content.
+  </Card.Text>
+  <Button variant="primary">Go somewhere</Button>
+</Card.Body>
+</Card>
+        </Col>
+      </Row>
       
-      </>
-     )
+      <h1 style={{textAlign:"center ", margin:"30px"}}>Explore new designs</h1>
+      </Container>
+       {/* ------------------------------------------------------------------------------------------------  */}
+      <div bg="red" >
+        <Container>
+        <Row>
+
+         <Col className='my-4 '>
+         <Image src="public/interior d/d5.avif" fluid width="350px"/> 
+         </Col>
+
+         <Col>
+              <h3 style={{marginTop:"100px",paddingBottom:"30px",fontFamily:"cursive"}}>Thoughtful & Functional</h3>
+              <p style={{fontSize:"18px"}}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Commodi voluptas voluptates quibusdam esse incidunt distinctio doloribus numquam nulla quisquam, sint quia nesciunt, consectetur totam reiciendis, minus eos quidem asperiores deserunt!</p>
+              <Button variant="primary">Explore</Button>
+        </Col>
+        
 
 
+        </Row>
+        <h1 style={{textAlign:"center",marginTop:"40px",paddingBottom:"40px"}}>Catogories </h1>
+        <Row>
+              <Col>
+                  <img src="public/interior d/dd3.jpg" alt="" width="300px"/>
+                 
 
+              </Col>
+             
 
-})
+              <Col>
+              <img src="public/interior d/dd9.webp" alt="" width="300px"/>
+                  
+              </Col>
 
-const App=()=>{
-  return (
-    <>
-    <h1>Table </h1>
-        <table border="2" width="500" bgcolor="pink">
-            <tr bgcolor="blue">
-              <th>Roll no</th>
-              <th>Name</th>
-              <th>City</th>
-
-            </tr>
-            {ans}
-        </table>
-    </>
-  )
-}
-export default App;
-*/
-
-// ==============================================================
-/*
-import EmployeeData from "./EmployeeData"
-
-const myans=EmployeeData.map((key)=>{
-           return(
-            <>
-                <tr>
-                  <th>{key.empno}</th>
-                  <th>{key.name}</th>
-                  <th>{key.salary}</th>
-                  <th>{key.city}</th>
-
-                </tr>
-            </>
-           )
-
-
-
-})
-
-const App=()=>{
-  return(
-    <>
-          <table  bgcolor="pink" width="500">
-                   <tr>
-                    <th>Employee no</th>
-                    <th>Employee Name</th>
-                    <th>Salary</th>
-                    <th>City</th>
-
-
-                   </tr>
-                {myans}
-          </table>
-    
-    </>
-  )
-}
-export default App;
-*/
-
-// ==============================22 JUne=============================
-/*
-import Cybromnew from "./Cybromnew";
-import Studentnew from "./Studentnew";
-
-const ans=Studentnew.map((key)=><Cybromnew rn={key.Rollno} nm={key.Name} ct={key.City} fs={key.Fees}/>)
-
-
-const App=()=>{
-  return(
-    <>
-         <h1>Our Student list</h1>
-         <table border={2} width={500} bgcolor="pink" align="center">
-                <tr bgcolor="yellow">
-                  <th>RollNo.</th>
-                  <th>Name.</th>
-                  <th>City</th>
-                  <th>Fees</th>
-
-                </tr>
-                {ans}
-         </table>
-    </>
-  )
-}
-export default App;
-*/
-
-// =========================================================================
-
-const App=()=>{
-  return(
-    <>
-       {/* <h1 style={{color:"yellow" , fontFamily:"verdana"}}>Welcome to cybrome classes</h1>
-       <h1 style={{color:"pink" , fontFamily:"verdana" , fontSize:"50px"}}>Welcome to cybrome classes</h1> */}
-
-       <div style={{backgroundColor:"red", borderRadius:"20px" , width:"200px", height:"200px" }} >
-
-             <div style={{backgroundColor:"green" ,borderRadius:"20px" , width:"150px" , height:"150px", position:"relative", top:"20px", left:"20px",}}>
-
-                     <div style={{backgroundColor:"blue" ,borderRadius:"20px" , width:"100px",height:"100px", margin:"10px",position:"relative", top:"20px", left:"20px" }}>
-
-                      <div style={{position:"relative" ,top:"40px" ,left:"20px", fontSize:"20px"}}>Cybrom</div>
+              <Col>
+                  <img src="public/interior d/dd10.webp" alt="" width="300px"/>
+                   
+              </Col>
+             </Row>
+             <Row className='my-3'>
+              <Col>
+                  <img src="public/interior d/dd7.jpg" alt="" width="300px"/>
                       
-                      </div>
-             </div>
-       </div>
-       
+              </Col>
 
+              
+              <Col>
+                  <img src="public/interior d/dd6.jpg" alt="" width="300px" height="200px"/>
+                   
+              </Col>
+
+              <Col>
+                  <img src="public/interior d/dd4.jpg" alt="" width="300px"/>
+                      
+              </Col>
+         </Row>
+        </Container>
+      </div>
+            
+      <div style={{textAlign:"center",marginTop:"60px",paddingBottom:"60px"}}>
+         <h1 style={{marginTop:"60px",paddingBottom:"60px"}}>Connect with us</h1>
+        <img src="public/bg/3.png" alt="" width="100%" height={"600px"}/>
+      </div>
+
+   
     </>
+
+
+    
+
   )
 }
 export default App;
+
+// ===================================================28 June====================================================
+
