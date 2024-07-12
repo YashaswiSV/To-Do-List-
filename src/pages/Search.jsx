51e23ffb86fdbf5e32a654fab83db262dc68c9ff
YashaswiSV <vermayashaswi1@gmail.com>
2024-07-12 23:51:23 +0530

    this is second commit

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,10 +1,50 @@
-
+import axios from "axios";
+import { useState } from "react";
+import Table from 'react-bootstrap/Table';
 
 const Search=()=>{
+
+    const [rno , setrno]=useState("");
+    const[mydata , setmydata]=useState([]);
+
+    const handlesearch=()=>{
+        let url=`http://localhost:3000/student/?Rollno=${rno}`
+        axios.get(url).then((res)=>{
+            setmydata(res.data)
+        })
+
+    }
+    let ans=mydata.map((key)=>{
+        return(
+            <>
+              <tr>
+                  <td>{key.Rollno}</td>
+                  <td>{key.name}</td>
+                  <td>{key.city}</td>
+                  <td>{key.fees}</td>
+
+              </tr>
+            
+            </>
+        )
+    })
     return(
         <>
-           
-           <h1>This is Search page</h1>
+           <input type="text" value={rno} onChange={(e)=>{setrno(e.target.value)}}/>
+          <button onClick={handlesearch}>search</button>
+          <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>Rollno</th>
+          <th>Name</th>
+          <th>City</th>
+          <th>Fees</th>
+        </tr>
+      </thead>
+      <tbody>
+         {ans}
+      </tbody>
+      </Table>
         
         </>
     )
